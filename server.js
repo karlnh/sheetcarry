622ad7b6feb23b60be1baf0fbab8e5496114d931
karlnh <hubigkarl@gmail.com>
2023-02-19 20:22:51 -0600

    add path, port & listen; correct handlebars const

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
-const expresHandlebars = require('express-handlebars');
+const expressHandlebars = require('express-handlebars');
 // const routes = require('');
 
 const app = express();
-const hbs = expresHandlebars.create();
+const PORT = process.env.PORT || 3001;
+const hbs = expressHandlebars.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -13,3 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
